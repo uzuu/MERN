98a68da50c6305287ebb847b6139170879c5fb68
uzuu <khishigt.kh@gmail.com>
2025-03-31 11:05:44 +0800

    Hero

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
