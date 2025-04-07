c0aaf915e39ff6cc0db34f43cb622f4881fe6612
uzuu <khishigt.kh@gmail.com>
2025-04-07 11:56:38 +0800

    CompanyLogo

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import CompanyLogo from './components/CompanyLogo'
 
 function App() {
 
 
   return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
+    <main className='relative min-h-screen overflow-x-hidden'>
+      <div className='absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'></div>
+      <div className='overflow-hidden'>
+        <Navbar />
+        <Hero />
+        <CompanyLogo />
+      </div>
+    </main>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
     // </div>
     <section className='container mx-auto flex flex-col md:flex-row pt-44 pb-6 px-4 sm:px-6 lg:px-8'>
       {/* left */}
-      <div className='w-full md:w-1/2 spacy-y-8'>
+      <div className='w-full md:w-1/2 space-y-8'>
         <div className='flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group'>
           <span className='text-blue-600 group-hover:scale-100 transition-transform'>★</span>
           <span className='text-sm font-medium'> Jump start your growth</span>
         </div>
-        <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold leading-tight'>We boos the growth for <span className='text-blue-600 relative inline-block'>Startup to Fortune 500
+        <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold leading-tight'>We boost the growth for <span className='text-blue-600 relative inline-block'>Startup to Fortune 500
           <span className='absolute bottom-0 left-0 w-full h-0.5 bg-blue-200/60'></span>
         </span>
           Companies<span className='inline-block ml-2 animate-pulse'>⏰</span></h1>
